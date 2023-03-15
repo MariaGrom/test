5ff63a58db4d40330c769367b7b5aca2a3cfeffe
Maria Gromyko <mazvereva91@mail.ru>
2023-03-15 20:10:30 +0300

    feat: successful registration page, subtim button state

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,14 @@
-const formElement = document.querySelector('.form');
-const formInput = formElement.querySelector('.form__input');
-const formError = formElement.querySelector(`.${formInput.id}-error`);
-
 // Функция, которая добавляет класс с ошибкой
 const showInputError = (formElement, inputElement, errorMessage) => {
-  
 	const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-
 	inputElement.classList.add('form__input_type_error');
 	errorElement.textContent = errorMessage;
-	
 	errorElement.classList.add('form__input-error_active');
 };
 
 // Функция, которая удаляет класс с ошибкой
 const hideInputError = (formElement, inputElement) => {
-
 	const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-
   inputElement.classList.remove('form__input_type_error');
 	errorElement.classList.remove('form__input-error_active');
 	errorElement.textContent = '';
@@ -28,59 +19,75 @@ const showInputSuccess = (formElement, inputElement) => {
 	inputElement.classList.add('form__input_type_success');
 };
 
-//Функция, которая изменяет текст инпута на красный, если есть ошибка
+// Функция, которая изменяет текст инпута на красный, если есть ошибка
 const showInputErrorText = (formElement, inputElement) => {
 	inputElement.classList.add('form__input_text_error');
-}
+};
 
-//Функция, которая возвращает цвет текста при исправлении 
+// Функция, которая возвращает цвет текста при исправлении 
 const hideInputErrorText = (formElement, inputElement) => {
-	inputElement.classList.remove('form__input_text_error')
-}
+	inputElement.classList.remove('form__input_text_error');
+};
 
 // Функция, которая проверяет валидность поля
 const isValid = (formElement, inputElement) => {
   if (!inputElement.validity.valid) {
-    // Если поле не проходит валидацию, покажем ошибку
     showInputError(formElement, inputElement, inputElement.validationMessage);
 		showInputErrorText(formElement, inputElement)
   } else {
-    // Если проходит, скроем
     hideInputError(formElement, inputElement);
 		showInputSuccess(formElement, inputElement);
 		hideInputErrorText(formElement, inputElement);
   }
 };
 
+// Функция проверки валидности полей формы
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  })
+}; 
 
+// Функция состояния кнопки submit
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add('form__button_type_inactive');
+  } else {
+    buttonElement.classList.remove('form__button_type_inactive');
+  }
+}; 
+
+//  Установим слушатель событий
 const setEventListeners = (formElement) => {
-  // Находим все поля внутри формы,
-  // сделаем из них массив методом Array.from
   const inputList = Array.from(formElement.querySelectorAll('.form__input'));
+	const buttonElement = formElement.querySelector('.form__button');
+	
+	toggleButtonState(inputList, buttonElement)
 
-  // Обойдём все элементы полученной коллекции
-  inputList.forEach((inputElement) => {
-    // каждому полю добавим обработчик события input
+	inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', () => {
-      // Внутри колбэка вызовем isValid,
-      // передав ей форму и проверяемый элемент
       isValid(formElement, inputElement)
+
+			toggleButtonState(inputList, buttonElement);
     });
   });
 }; 
 
 const enableValidation = () => {
-  // Найдём все формы с указанным классом в DOM,
-  // сделаем из них массив методом Array.from
   const formList = Array.from(document.querySelectorAll('.form'));
+	formList.forEach((formElement) => {
+		formElement.addEventListener('submit', (evt)=> {
+			evt.preventDefault();
+		})
+				setEventListeners(formElement);
+			});
+		};
 
-  // Переберём полученную коллекцию
-  formList.forEach((formElement) => {
-    // Для каждой формы вызовем функцию setEventListeners,
-    // передав ей элемент формы
-    setEventListeners(formElement);
-  });
-};
-
-// Вызовем функцию
 enableValidation(); 
+
+
+
+// Функция всплывающего окна (при необходимости заменить код на onclick кнопки отправки формы)
+function showRegistration () {
+	alert("Регистрация прошла успешно")
+}
